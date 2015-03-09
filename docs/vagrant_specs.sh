c6eda409710076e370f03a283a2bbf9b4a34e239
Michel Blanc <mb@mbnet.fr>
2015-03-09 17:19:17 +0100

    Readded git install
    git has to be installed sicne it is not present on base image

--- docs/vagrant_specs.sh
+++ docs/vagrant_specs.sh
@@ -13,16 +13,17 @@
 #
 # vagrant ssh -c ./specs
 #
-if [ "x$1" == "x--install" ]; then
-  cp ~vagrant/specs /usr/local/bin/specs
-  chmod 755 /usr/local/bin/specs
-  su vagrant -c 'git clone --depth 1 https://github.com/nickjj/rolespec'
-  cd ~vagrant/rolespec && make install
-  su vagrant -c 'rolespec -i ~/testdir'
-  su vagrant -c "ln -s /vagrant/ ~/testdir/roles/$2"
-  su vagrant -c "git clone $3 ~/testdir/tests"
-  exit
-fi
+  if [ "x$1" == "x--install" ]; then
+    cp ~vagrant/specs /usr/local/bin/specs
+    chmod 755 /usr/local/bin/specs
+    sudo apt-get install -qqy git
+    su vagrant -c 'git clone --depth 1 https://github.com/nickjj/rolespec'
+    cd ~vagrant/rolespec && make install
+    su vagrant -c 'rolespec -i ~/testdir'
+    su vagrant -c "ln -s /vagrant/ ~/testdir/roles/$2"
+    su vagrant -c "git clone $3 ~/testdir/tests"
+    exit
+  fi
 
 # if [ "x$1" == "x--galaxy" ]; then
 #   # Remove anything below ~/testdir/roles
